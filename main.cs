a3e07692c08841f865f0dba398c18e71bdee7cb1
AluminumChassis <32084215+AluminumChassis@users.noreply.github.com>
2018-09-13 20:00:00 -0700

    commit

--- main.cs
+++ main.cs
@@ -3,39 +3,78 @@ using System.IO;
 using System.Collections.Generic;
 
 using System.Threading;
-namespace aiNameSpace
+namespace AI
 {
 
     class neuralData {
-        public List<object[]> inputData;
-        public List<object> outputData;
 
         public Type objectData;
-        public neuralData(List<object[]> input, List<object> output) {
-            objectData = input[0].GetType();
-            inputData = input;
-            outputData = output;
+        public neuralData(List<int[]> input, List<int> output) {
+            objectData = typeof(int);
+            List<int[]> inputData = input;
+            List<int> outputData = output;
+        }
+        public neuralData(List<float[]> input, List<float> output)
+        {
+            objectData = typeof(float);
+            List<float[]> inputData = input;
+            List<float> outputData = output;
         }
     }
     class neuralNetwork {
         public int max;
+        public bool flexible;
         public neuralData data;
+        public List<node> nodes;
         public bool useMemory;
         public neuralNetwork(neuralData Data, int maxNumOfNodes) {
+            Console.WriteLine(Data);
             max = maxNumOfNodes;
             data = Data;
+            nodes.Add(new node(data.objectData, false));
+            nodes.Add(new node(data.objectData, true));
         }
-        public cull() {
-
+        public neuralNetwork(neuralData Data, bool flexible)
+        {
+            flexible = true;
+            data = Data;
+            nodes.Add(new node(data.objectData, false));
+            nodes.Add(new node(data.objectData, true));
+        }
+        public void createNode(int pos) {
+            nodes.Insert(pos, new node(data.objectData, false));
+        }
+        public void testNodes() {
+        
         }
     }
     class node {
         public Type input;
+        public bool isFinal = false;
         public Type output;
-        public node(Type input, Type output)
+        public node nextNode;
+        //public List<>
+        public node(Type input, Type output, bool final)
         {
+            isFinal = final;
             this.input = input;
             this.output = output;
+        }
+        public node(Type dataType, bool final)
+        {
+            isFinal = final;
+            this.input = dataType;
+            this.output = dataType;
+        }
+        public void act(int[] input) {
+            input[0]++;
+            nextNode.act(input);
+        }
+
+        public void act(string[] input)
+        {
+            input[0]+=" ";
+            nextNode.act(input);
         }
     }
 }
--- test.cs
+++ test.cs
@@ -1,15 +1,19 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
-namespace aiNameSpace
+namespace AI
 {
     class App
     {
+        public static List<int[]> input = new List<int[]> { new int[] { 0, 1, 2 }, new int[] { 1, 2, 3 } };
+        public static List<int> output = new List<int> { 0, 1, 2 };
+
         public static void Main(string[] args)
         {
-            neuralData data = new neuralData();
-            neuralNetork net = new neuralNetwork(data);
-
+            neuralData data = new neuralData(input, output);
+            neuralNetwork net = new neuralNetwork(data, 1000);
+            net.createNode(1);
+            Console.WriteLine(net.nodes);
             Console.ReadLine();
         }
 
